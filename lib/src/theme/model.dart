fa1aa3c5a70ab8305cbff1587c53019d07b95841
KalilDev <kalilgamer0@gmail.com>
2021-11-27 19:26:04 -0300

    theme: model: hash, equality, copy with and lerp

--- lib/src/theme/model.dart
+++ lib/src/theme/model.dart
@@ -11,6 +11,25 @@ class Themes {
     this.darkTheme,
     this.monetTheme,
   );
+
+  int get hashCode => Object.hashAll([
+        lightTheme,
+        darkTheme,
+        monetTheme,
+      ]);
+
+  bool operator ==(other) {
+    if (identical(other, this)) {
+      return true;
+    }
+    if (other is! Themes) {
+      return false;
+    }
+    return true &&
+        lightTheme == other.lightTheme &&
+        darkTheme == other.darkTheme &&
+        monetTheme == other.monetTheme;
+  }
 }
 
 class MD3ThemeData {
@@ -25,6 +44,52 @@ class MD3ThemeData {
     required this.elevation,
     required this.stateLayerOpacity,
   });
+
+  int get hashCode => Object.hashAll([
+        colorTheme,
+        textTheme,
+        elevation,
+        stateLayerOpacity,
+      ]);
+
+  bool operator ==(other) {
+    if (identical(other, this)) {
+      return true;
+    }
+    if (other is! MD3ThemeData) {
+      return false;
+    }
+    return true &&
+        colorTheme == other.colorTheme &&
+        elevation == other.elevation &&
+        stateLayerOpacity == other.stateLayerOpacity;
+  }
+
+  MD3ThemeData copyWith({
+    MonetTheme? colorTheme,
+    MD3TextTheme? textTheme,
+    MD3ElevationTheme? elevation,
+    MD3StateLayerOpacityTheme? stateLayerOpacity,
+  }) =>
+      MD3ThemeData(
+        colorTheme: colorTheme ?? this.colorTheme,
+        textTheme: textTheme ?? this.textTheme,
+        elevation: elevation ?? this.elevation,
+        stateLayerOpacity: stateLayerOpacity ?? this.stateLayerOpacity,
+      );
+
+  static MD3ThemeData lerp(MD3ThemeData a, MD3ThemeData b, double t) {
+    assert(a != null);
+    assert(b != null);
+    assert(t != null);
+    return MD3ThemeData(
+      colorTheme: MonetTheme.lerp(a.colorTheme, b.colorTheme, t),
+      textTheme: MD3TextTheme.lerp(a.textTheme, b.textTheme, t),
+      elevation: MD3ElevationTheme.lerp(a.elevation, b.elevation, t),
+      stateLayerOpacity: MD3StateLayerOpacityTheme.lerp(
+          a.stateLayerOpacity, b.stateLayerOpacity, t),
+    );
+  }
 }
 
 enum MD3WindowSizeClass {
